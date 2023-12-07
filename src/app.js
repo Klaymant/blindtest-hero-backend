a4c78588a749d8fb6e19c64b1e7672d8fdd1f681
klaymant <clement.gauthier@protonmail.com>
2023-12-07 13:37:00 +0100

    Added an endpoint to get multiple chart tracks

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ router.use((_, res, next) => {
 });
 
 router.get('/chart/track/:index', getChartTrackByIndex);
+router.get('/chart/tracks/:limit', getChartTracks);
 
 app.use(rateLimitMiddleware);
 app.use('/', router); 
@@ -39,4 +40,20 @@ async function getChartTrackByIndex(req, res) {
     res.status(404).send('No data found');
   else
     res.send(data.tracks.data[0]);
+}
+
+async function getChartTracks(req, res) {
+  const searchParams = new URLSearchParams({
+    limit: String(+req.params.limit + 1),
+  });
+
+  const response = await fetch(CONFIG.deezerApiUri + '/chart/0/tracks' + '?' + searchParams.toString());
+  const data = await response.json();
+
+  if (data.error)
+    res.status(500).send(data.error.message);
+  else if (!data?.data)
+    res.status(404).send('No data found');
+  else
+    res.send(data.data);
 }
